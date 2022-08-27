a0787f77c9a0fd292987f9f7d2abe8b39f0bf6f6
Julian <99680778+YuKorostelev@users.noreply.github.com>
2022-08-28 00:08:21 +0300

    Update Stayatest.js

--- Stayatest.js
+++ Stayatest.js
@@ -3,8 +3,8 @@ describe('Тестирование формы логина и пароля Staya
 	it('Тестирование формы логина и пароля Staya', function() {
 		cy.visit('https://Staya.dog');
 		cy.get('.header-bottom__right--link').click();
-		cy.get('.auth-form > form > [type="email"]').type('vis-julian-korostelev@qa.studio');
-		cy.get('.auth-form > form > [type="password"]').type('JulianTest96');
+		cy.get('.auth-form > form > [type="email"]').type('email');
+		cy.get('.auth-form > form > [type="password"]').type('password');
 		cy.get('.auth-form__submit > .s-button__content').click();
 		cy.contains('Мои заказы');
 	})
