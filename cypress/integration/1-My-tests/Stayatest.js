9c2488fae2aeb1ba6ef54794e8aa20bf80d6040f
Julian <99680778+YuKorostelev@users.noreply.github.com>
2022-08-29 23:50:21 +0300

    Update Stayatest.js

--- cypress/integration/1-My-tests/Stayatest.js
+++ cypress/integration/1-My-tests/Stayatest.js
@@ -1,12 +1,12 @@
 
-//Позитивный и негативный тест формы логина и пароля
+//Позитивный и негативный тест формы логина и пароля с обезличенным эмейлом и паролем
 describe('Позитивный тест формы логина и пароля Staya', function () {
 
 	it('Ввод корректного эмейла и пароля', function() {
 		cy.visit('https://Staya.dog');
 		cy.get('.header-bottom__right--link').click();
-		cy.get('.auth-form > form > [type="email"]').type('vis-julian-korostelev@qa.studio');
-		cy.get('.auth-form > form > [type="password"]').type('JulianTest96');
+		cy.get('.auth-form > form > [type="email"]').type('correctemail');
+		cy.get('.auth-form > form > [type="password"]').type('password');
 		cy.get('.auth-form__submit > .s-button__content').click();
 		cy.contains('Мои заказы');
 	})
@@ -16,7 +16,7 @@ describe('Негативный тест формы логина и пароля 
 	it('Ввод корректного эмейла и некорректного пароля', function() {
 		cy.visit('https://Staya.dog');
 		cy.get('.header-bottom__right--link').click();
-		cy.get('.auth-form > form > [type="email"]').type('vis-julian-korostelev@qa.studio');
+		cy.get('.auth-form > form > [type="email"]').type('correctemail');
 		cy.get('.auth-form > form > [type="password"]').type('incorrectpassword');
 		cy.get('.auth-form__submit > .s-button__content').click();
 		cy.get('.error-label');
