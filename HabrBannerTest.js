23b3399a1d9e5971c222193c0bd8274ad020dede
Julian <99680778+YuKorostelev@users.noreply.github.com>
2022-08-29 22:53:22 +0300

    Update HabrBannerTest.js

--- HabrBannerTest.js
+++ HabrBannerTest.js
@@ -1,44 +1,44 @@
 //Проверил наличие и отображение рекламного баннера во вкладках на главной странице Хабра
 describe('Проверка рекламного баннера на Хабре', function () {
 
-	it('Проверка баннера вкладка Все потоки', function() {
+	it('Вкладка Все потоки', function() {
 		cy.visit('https://habr.ru');
 		cy.get('[href="/en/all/"]').click();
 		cy.wait(3000);
 		cy.get('.tm-layout-sidebar__ads');
 	})
 
-	it('Проверка баннера вкладка Разработка', function() {
+	it('Вкладка Разработка', function() {
 		cy.get('[href="/en/flows/develop/"]').click();
 		cy.wait(3000);
 		cy.get('.tm-layout-sidebar__ads');
 	})
 
-    it('Проверка баннера вкладка Администрирование', function() {
+        it('Вкладка Администрирование', function() {
 		cy.get('[href="/en/flows/admin/"]');
 		cy.wait(3000);
 		cy.get('.tm-layout-sidebar__ads');
 	})
 
-	it('Проверка баннера вкладка Дизайн', function() {
+	it('Вкладка Дизайн', function() {
 		cy.get('[href="/en/flows/design/"]');
 		cy.wait(3000);
 		cy.get('.tm-layout-sidebar__ads');
 	})
 
-	it('Проверка баннера вкладка Менеджмент', function() {
+	it('Вкладка Менеджмент', function() {
 		cy.get('[href="/en/flows/management/"]');
 		cy.wait(3000);
 		cy.get('.tm-layout-sidebar__ads');
 	})
 
-	it('Проверка баннера вкладка Маркетинг', function() {
+	it('Вкладка Маркетинг', function() {
 	    cy.get('[href="/en/flows/marketing/"]');
 	    cy.wait(3000);
 	    cy.get('.tm-layout-sidebar__ads');
-    })
+         })
 
-    it('Проверка баннера вкладка НаучПоп', function() {
+        it('Вкладка НаучПоп', function() {
 	    cy.get('[href="/en/flows/popsci/"]');
 	    cy.wait(3000);
 	    cy.get('.tm-layout-sidebar__ads');
